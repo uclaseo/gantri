9a5af4d79e4edd77a7e9711cd93596ecaa977ae5
Inseok Seo <inseok.ucla@gmail.com>
2019-06-24 14:28:52 -0700

    Added all database relations and fixed returned objects

--- controllers/art.controller.js
+++ controllers/art.controller.js
@@ -1,19 +1,78 @@
-const { Art } = require('../databases/connection');
+const { Art, Comments } = require('../databases/connection');
 
 const ArtController = {
   get: async (req, res) => {
-    console.log('get');
-    // Art.findAll().then(art => {
-    //   console.log(art);
-    // })
+    try {
+      console.log('get');
+      const allArts = await Art.findAll();
+      return res.status(200).send(allArts);
+    } catch (error) {
+      console.error('get error: ', error);
+      return res.send(error);
+    }
   },
 
   getById: async (req, res) => {
-    console.log('getbyid');
+    try {
+      const { id } = req.params;
+      const art = await Art.findOne({
+        where: { id },
+        include: [
+          {
+            model: Comments,
+            attributes: ['id', 'name', 'content', 'userId']
+          }
+        ]
+      });
+      return res.status(200).send(art);
+    } catch (error) {
+      console.error('getById error: ', error);
+      return res.send(error);
+    }
   },
 
   addComment: async (req, res) => {
-    console.log('addComment');
+    try {  
+      const { id } = req.params;
+      const { userID, name, content } = req.body;
+
+
+      if (userId) {
+        const comment = Comments.create({
+          userId: userID,
+          artId: id,
+          name,
+          content,
+        });
+        return res.status(200).send(comment);
+      }
+
+      const art = await Art.findOne({
+        where: { id },
+        include: {
+          model: Comments,
+          attributes: ['name'],
+        },
+      });
+      let nameExists = false;
+      art.comments.forEach((comment) => {
+        if (comment.name === name) {
+          nameExists = true;
+        }
+      });
+      if (!nameExists) {
+        const comment = Comments.create({
+          artId: id,
+          name,
+          content,
+        });
+        return res.status(200).send(comment);
+      }
+
+    } catch (error) {
+      console.error('addComment error: ', error);
+      return res.send(error);
+    }
   }
 };
 
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,11 +2,30 @@ const { Users } = require('../databases/connection');
 
 const UserController = {
   getUsers: async (req, res) => {
-    console.log('getUsers');
+    try {
+      const users = await Users.findAll();
+      return res.status(200).send(users);
+    } catch (error) {
+      console.error('getUsers error: ', error);
+      return res.send(error);
+    }
   },
 
   createUser: async (req, res) => {
-    console.log('createUser');
+    try {
+      const { name, age, location } = req.body;
+      const [ user, isCreated ] = await Users.findOrCreate({
+        where: {
+          name,
+          age,
+          location,
+        }
+      });
+      return res.status(200).send(user);
+    } catch (error) {
+      console.error('createUser error: ', error);
+      return res.send(error);
+    }
   },
 };
 
