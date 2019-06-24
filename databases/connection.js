98ba62cb560ec9b16664e8925b781b8183971eb1
Inseok Seo <inseok.ucla@gmail.com>
2019-06-24 13:07:22 -0700

    Imported csv into sql database

--- databases/connection.js
+++ databases/connection.js
@@ -1,4 +1,12 @@
 const Sequelize = require('sequelize');
+const fs = require('fs');
+const csv = require('csv');
+const input = fs.createReadStream('./the-tate-collection.csv');
+const parser = csv.parse({
+  delimiter: ';',
+  columns: true
+});
+
 
 const sequelize = new Sequelize('gantri', 'root', '', {
   host: 'localhost',
@@ -8,59 +16,93 @@ const sequelize = new Sequelize('gantri', 'root', '', {
   }
 });
 
-  sequelize
-    .authenticate()
-    .then(() => {
-      console.log('Connection has been established successfully.');
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
+const Art = sequelize.define('art', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+  },
+  artist: {
+    type: Sequelize.STRING,
 
-    })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
+  },
+  year: {
+    type: Sequelize.INTEGER,
+  },
+  comments: {
+    type: Sequelize.INTEGER,
+  }
+});
+
+const Users = sequelize.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+  }
+});
+
+const Comments = sequelize.define('comments', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  content: {
+    type: Sequelize.STRING,
+  }
+});
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    const transform = csv.transform((row) => {
+      const resultObject = {
+        id: row['id'],
+        // accession_number: row['accession_number'],
+        artist: row['artist'],
+        // artistRole: row['artistRole'],
+        // artistId: row['artistId'],
+        title: row['title'],
+        // dateText: row['dateText'],
+        // medium: row['medium'],
+        // creditLine: row['creditLine'],
+        year: row['year'],
+        // acquisitionYear: row['acquisitionYear'],
+        // dimensions: row['dimensions'],
+        // width: row['width'],
+        // height: row['height'],
+        // depth: row['depth'],
+        // units: row['units'],
+        // inscription: row['inscription'],
+        // thumbnailCopyright: row['thumbnailCopyright'],
+        // thumbnailUrl: row['thumbnailUrl'],
+        // url: row['url'],
+      };
+      Art.create(resultObject)
+        .then(() => {
+          console.log('CSV imported Successfully');
+        })
+        .catch((error) => {
+          console.log('CSV is not imported: ', error)
+        })
     });
-
-    const Art = sequelize.define('art', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      title: {
-        type: Sequelize.STRING,
-      },
-      artist: {
-        type: Sequelize.STRING,
-    
-      },
-      year: {
-        type: Sequelize.INTEGER,
-      },
-      comments: {
-        type: Sequelize.INTEGER,
-      }
-    });
-
-    const Users = sequelize.define('users', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-      }
-    });
-
-    const Comments = sequelize.define('comments', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      content: {
-        type: Sequelize.STRING,
-      }
-    });
+    input.pipe(parser).pipe(transform);
+  });
 
 module.exports = {
   sequelize,
